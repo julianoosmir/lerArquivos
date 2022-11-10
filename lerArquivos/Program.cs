a0e17b728c880e75b0b7c0480a1f4cb3f11e4248
PMCGAD\juliano.hein <juliano.hein@agetec.campogrande.ms.gov.br>
2022-11-10 11:13:29 -0400

    Adicionar arquivos de projeto.

---
+++ lerArquivos/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string projetos = @"C:\Users\juliano.hein\Documents\projetos\passe2020\build";
+
+string projetos2 = @"C:\Users\juliano.hein\Documents\projetos2\passe2020\build";
+
+List<string> arquivos = listar(projetos);
+List<string> arquivos2 = listar(projetos2);
+Diferenciar(arquivos, arquivos2);
+
+void Diferenciar(List<string> arquivos, List<string> arquivos2)
+{
+    List<string> diff = arquivos.Except(arquivos2).ToList();
+
+    diff.ForEach(x => Console.WriteLine(x));
+}
+
+List<string> listar(string filename)
+{
+    List<string> lita = new List<string>();
+    DirectoryInfo Dir = new DirectoryInfo(filename);
+    // Busca automaticamente todos os arquivos em todos os subdiretórios
+    FileInfo[] Files = Dir.GetFiles("*", SearchOption.AllDirectories);
+    foreach (FileInfo File in Files)
+    {        
+        lita.Add(File.Name);
+    }
+    Console.WriteLine(lita.Count);
+    return lita;
+}
+
